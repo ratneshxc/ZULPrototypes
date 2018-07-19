09024878fc0b7d094940fbf26b7108fb2c55afb5
shakti <svpsctg11@yahoo.com>
2018-07-19 11:14:53 +0530

    Adds Dashboard route

--- src/config/Route.js
+++ src/config/Route.js
@@ -3,10 +3,12 @@ import {
   } from 'react-navigation';
   import Login from '../components/login/Login';
   import Assessment from '../components/assessment/Assessment';
+  import Dashboard from "../components/dashboard/Dashboard";
   
   const AppRoute = createSwitchNavigator({
     Login: { screen: Login },
     Assessment: { screen: Assessment },
+    Dashboard:{screen: Dashboard}
   });
   
   export default AppRoute;
