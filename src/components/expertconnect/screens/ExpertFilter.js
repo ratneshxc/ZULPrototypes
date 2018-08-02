ece7d074eb0e0a1b64634815c92f5f3f0854ddb4
shakti <svpsctg11@yahoo.com>
2018-08-02 14:14:47 +0530

    Make Skeleton layout for the react code

--- src/components/expertconnect/screens/ExpertFilter.js
+++ src/components/expertconnect/screens/ExpertFilter.js
@@ -1,123 +1,38 @@
 
 import FilterHeader from '../components/FilterHeader';
 import React, { Component } from 'react';
-
-import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
-
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import expertListData from '../../../data/expertConnectListData';
-
-import * as Progress from 'react-native-progress';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Card, CardItem} from 'native-base';
-import Favorite from '../components/FavoriteAction';
-import MailAction from '../components/MailAction';
-import BookAction from '../components/BookAction';
 class FlatListItem extends Component {
 
   _onPressButton(e) {
 
-      console.log('Button pressed');
+    console.log('Button pressed');
 
   }
 
   render() {
 
-      return (
+    return (
 
-          <View >
+      <View >
+        <View style={{ flex: 2, flexDirection: 'column' }}>
 
-              <Card style={{
-                  padding: 2,
-              }}>
+          <View style={[{ flexDirection: 'row' }, styles.titleStyle]}>
+           <Text style={[styles.textStyle, { fontWeight: 'bold' }]}>{this.props.item.name}</Text>
+         </View>
 
-                  <CardItem style={{
-
-                      flex: 2,
-                      padding: 2,
-                      flexDirection: 'row',
-
-
-
-
-
-                  }}>
-                      <View style={{ flexDirection: 'column', marginRight: 10 }}>
-                          <Image style={{ width: 50, height: 50, borderRadius: 100 }} source={require('../../../assests/images/suresh.png')} />
-                          <Text style={[{ color: "#28a745", marginTop: 9 }]}>{this.props.item.availableON}</Text>
-                          <Text style={[{ color: "black" }]}>{this.props.item.fee}</Text>
-
-
-
-
-                      </View>
-
-                      <View style={{ flex: 2, flexDirection: 'column' }}>
-
-                          <View style={{ flexDirection: 'row' }}>
-
-                              <Text style={[styles.textStyle, { fontWeight: 'bold' }]}>{this.props.item.name}</Text>
-
-
-
-                          </View>
-                          <View style={{ flexDirection: 'row' }}>
-
-                              <Text style={styles.textStyle}>{this.props.item.education}</Text>
-
-
-
-                          </View>
-                          <View style={{ flexDirection: 'row' }}>
-
-                              <Text style={styles.textStyle}>{this.props.item.designation}</Text>
-
-
-
-                          </View>
-
-                          <View style={{ flexDirection: 'row' }}>
-
-                              <Text style={styles.textStyle}>{this.props.item.experience}</Text>
-
-
-
-                          </View>
-                          <View style={{ flexDirection: 'row' }}>
-
-                              <Text style={styles.textStyle}>{this.props.item.location}</Text>
-
-
-
-                          </View>
-
-
-
-
-                      </View>
-                      <View style={{ flexDirection: 'column', marginLeft: 5 }}>
-                          <Text style={{ color: "#000" }}>{this.props.item.reviewSummary.positiveVote}%</Text>
-                          <Progress.Bar style={{ borderRadius: 0, borderWidth: 0, marginLeft: -1, padding: 0 }} progress={this.props.item.reviewSummary.progress} width={80} color={this.props.item.reviewSummary.progressColor} unfilledColor={'#AAAAAA'} />
-                          <Text style={{ color: "#000" }}>{this.props.item.reviewSummary.totalVotes} Votes</Text>
-                          <Text
-                              style={{ color: "#17a2b8", textDecorationStyle: "solid", textDecorationLine: "underline" }}>
-                              {this.props.item.reviewSummary.totalReviews} Reviews
-                               </Text>
-                          <View style={{ flexDirection: 'row',marginTop:20 }}>
-                              <Favorite />
-                              <MailAction />
-                              <BookAction />
-
-                          </View>
-
-
-                      </View>
-                  </CardItem>
-              </Card>
-
-
+          <View style={[{ flexDirection: 'row' },styles.elemenStyle]}>
+          <Text style={styles.textStyle}>{this.props.item.education}</Text>
+          </View>
+          <View style={[{ flexDirection: 'row' },styles.elemenStyle]}>
+          <Text style={styles.textStyle}>{this.props.item.education}</Text>
           </View>
 
-      );
+       </View>
+      </View>
+
+    );
 
   }
 
@@ -138,7 +53,8 @@ export default class ExpertFilter extends Component {
 
             }}
 
-          ></FlatList>
+          >
+          </FlatList>
 
         </View>
       </View>
@@ -148,31 +64,24 @@ export default class ExpertFilter extends Component {
 
 
 const styles = StyleSheet.create({
-  BackgroundImage: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
+  titleStyle:{
+    backgroundColor:'#dddddd52',
+    height:40,
+    paddingTop:5,
+    paddingLeft:5
+   
+
+  },
+  elemenStyle:{
+    backgroundColor:'#ffffff',
+    height:40,
+    paddingTop:5,
+    paddingLeft:5
+
   },
   flatListItem: {
     flex: 1,
     backgroundColor: '#dddddd52'
-  },
-  container: {
-
-    flex: 1,
-
-    justifyContent: "space-evenly",
-
-    padding: 10,
-
-    right: 10
-
-  },
-  textStyle: {
-    flex: 5,
-    fontFamily: 'Arial',
-    color: "#222",
-    fontSize: 15,
   }
 
 });
