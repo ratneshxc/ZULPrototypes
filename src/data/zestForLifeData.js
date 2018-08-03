39de679c5e46cc65a4ce5df955ced1a198d755ec
ratneshxc <ratneshxc@gmail.com>
2018-08-03 12:52:36 +0530

    question json

---
+++ src/data/zestForLifeData.js
@@ -0,0 +1,374 @@
+var statementData = [
+    {  
+        no:1,
+        Category:"Zest For Life",
+        statement: "Do you try breathing techniques to relax, especially during a stressful situation?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I do not practice any breathing techniques", value: '1'},
+            {label:"I do not know any breathing techniques", value: '2'},
+            {label:"All the time", value: '3'},
+            {label:"Sometimes", value: '4'}
+        ],
+        ansType: 'single',
+        progressBar: 0.05,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:2,
+        Category:"Zest For Life",
+        statement: "Do you do rhythmic breathing to help your body cope with restrictions naturally?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I do not practise such things", value: '1'},
+            {label:"I do not know any breathing techniques", value: '2'},
+            {label:"Sometimes", value: '3'},
+            {label:"All the time", value: '4'}
+        ],
+        ansType: 'single',
+        progressBar: 0.1,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:3,
+        Category:"Zest For Life",
+        statement: "Do you use your breathing as a means of centering and living fully in the present moment?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I do not practise such things", value: '1'},
+            {label:"I do not know any breathing techniques", value: '1'},
+            {label:"Sometimes", value: '1'},
+            {label:"All the time", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.15,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:4,
+        Category:"Zest For Life",
+        statement: "Do you wear natural fabrics like cotton or wool ?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"Cotton and wool are not part of my clothing", value: '1'},
+            {label:"I sometimes wear them", value: '1'},
+            {label:"I try to wear them as much as possible", value: '1'},
+            {label:"I wear cotton and wool when necessary", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.2,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:5,
+        Category:"Zest For Life",
+        statement: "Is there sufficient natural light present in your house/office?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"Not at all", value: '1'},
+            {label:"Sometimes", value: '1'},
+            {label:"Most of the time", value: '1'},
+            {label:"All the time", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.25,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:6,
+        Category:"Zest For Life",
+        statement: "Do you prefer to stay in natural environment or in rooms with air conditioners?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"Always in AC rooms", value: '1'},
+            {label:"Most of the time in AC rooms", value: '1'},
+            {label:"Most of the time in natural environment", value: '1'},
+            {label:"All the time in natural environment", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.3,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:7,
+        Category:"Zest For Life",
+        statement: "Do you take massages to increase your level of wellness?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I never go for massages", value: '1'},
+            {label:"I sometimes go for a massage", value: '1'},
+            {label:"I go for massage frequently", value: '1'},
+            {label:"I regularly go for massages", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.35,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:8,
+        Category:"Zest For Life",
+        statement: "Are you aware of your thoughts?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"Never", value: '1'},
+            {label:"Rarely", value: '1'},
+            {label:"Sometimes", value: '1'},
+            {label:"Always", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.4,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:9,
+        Category:"Zest For Life",
+        statement: "Do you know your strengths and weakness?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I am not aware of them", value: '1'},
+            {label:"I am still understanding my strengths and weaknesses", value: '1'},
+            {label:"I am partially aware", value: '1'},
+            {label:"I am completely aware", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.45,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+
+    },
+    {  
+        no:10,
+        Category:"Zest For Life",
+        statement: "Do you take a vacation despite your tight schedule?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I never get time for vacations", value: '1'},
+            {label:"I go once in a year", value: '1'},
+            {label:"I go once in six months", value: '1'},
+            {label:"I go three or more times in a year", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.5,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:11,
+        Category:"Zest For Life",
+        statement: "Do you set goals for personal improvement?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I am perfect and do no need to improve", value: '1'},
+            {label:"Sometimes when I feel the need to improve", value: '1'},
+            {label:"Most of the time", value: '1'},
+            {label:"All the time", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.55,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:12,
+        Category:"Zest For Life",
+        statement: "How do you approach every situation of your life?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I assume the worst even before I approach a situation", value: '1'},
+            {label:"I let things to take its own course", value: '1'},
+            {label:"I positively approach every situation", value: '1'},
+            {label:"I believe that there is good in every situation", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.6,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:13,
+        Category:"Zest For Life",
+        statement: "How do you deal with competition at work?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I become extremly competitive", value: '1'},
+            {label:"I try to approach positively but fail to do so", value: '1'},
+            {label:"I am competitive but try not to hurt anyone", value: '1'},
+            {label:"I positively approach competition", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.65,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:14,
+        Category:"Zest For Life",
+        statement: "Are you passionate about your work?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I am not sure what I am passionate about", value: '1'},
+            {label:"I am passionate about things other than my work", value: '1'},
+            {label:"I try and be passionate about my work", value: '1'},
+            {label:"I am extremly passionate about my work", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.7,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:15,
+        Category:"Zest For Life",
+        statement: "Are there unfulfilled goals that you are passionate about?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I do not have any passionate goals", value: '1'},
+            {label:"I have many goals which are unfulfilled", value: '1'},
+            {label:"I am working towards fulfilling my goals", value: '1'},
+            {label:"All my goals are fullfilled or about to be fullfilled", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.75,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:16,
+        Category:"Zest For Life",
+        statement: "Are you excited about your daily activities?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I am not excited about anything", value: '1'},
+            {label:"I am rarely excited about things I do", value: '1'},
+            {label:"I try and be excited about things I do", value: '1'},
+            {label:"I am always excited about things I do", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.8,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:17,
+        Category:"Zest For Life",
+        statement: "Are you clear with what you want in life?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"I am not clear about what I want in life", value: '1'},
+            {label:"Sometimes", value: '1'},
+            {label:"Most of the time", value: '1'},
+            {label:"Almost always", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.85,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:18,
+        Category:"Zest For Life",
+        statement: "Do you over commit at work?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"All the time", value: '1'},
+            {label:"More times than I should", value: '1'},
+            {label:"Sometimes when I have to", value: '1'},
+            {label:"I only take up things that I can manage", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.9,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:19,
+        Category:"Zest For Life",
+        statement: "Do you feel guilty, while you are not working?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"To a great extent", value: '1'},
+            {label:"To some extent", value: '1'},
+            {label:"When I have taken up work and not fullfilled it", value: '1'},
+            {label:"No I do not feel guilty", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 0.95,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    },
+    {  
+        no:20,
+        Category:"Zest For Life",
+        statement: "Is work negatively influencing your health?",
+        imageURL: require('../../assests/images/illness-image.png'),
+        statementSuggestion:"",
+        options:[
+            {label:"To a great extent", value: '1'},
+            {label:"To some extent", value: '1'},
+            {label:"No, I am able to balance in order not to negatively effect my health", value: '1'},
+            {label:"My work does not affect my health", value: '1'}
+        ],
+        ansType: 'single',
+        progressBar: 1,
+        progressBarColor: '#ff2626',
+        likes: 25,
+        selectedIndex: null
+    }
+    ]
+    
+    module.exports = statementData;
+    
