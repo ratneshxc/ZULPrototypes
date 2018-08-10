0f68a9e076872dd11c76dd9dea3a51a490de1a22
shakti <svpsctg11@yahoo.com>
2018-08-10 18:17:34 +0530

    Refactor Assesment stats page

--- src/components/report/AssesmentStats.js
+++ src/components/report/AssesmentStats.js
@@ -1,37 +1,58 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import InfoAction from './InfoAction';
 import AssesmentSummaryLayout from './AssesmentSummaryLayout';
 export default class AssesmentStatus extends Component {
   render() {
     return (
       <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          marginTop: 5,
-          backgroundColor: "#ffffff",
-          // backgroundColor: '#dddddd52'
-        }}
+        style={styles.container}
       >
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <Text style={{ flex: 1, color: '#000000', fontWeight: 'bold', fontSize: 16 }}> Summary</Text>
-          <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.summaryText}>
+            Summary
+          </Text>
+          <View style={styles.actionArea}>
             <InfoAction />
 
           </View>
         </View>
         <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-          }}
+          style={styles.assesmentLayout}
         >
-
-          <AssesmentSummaryLayout stats={this.props.stats}/>
+          <AssesmentSummaryLayout stats={this.props.stats} />
         </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 5,
+    backgroundColor: "#ffffff",
+    // backgroundColor: '#dddddd52'
+  },
+  headerContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  summaryText: {
+    flex: 1,
+    color: '#000000',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  actionArea: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  assesmentLayout: {
+    flex: 1,
+    flexDirection: 'row',
+  }
+
+
+});
