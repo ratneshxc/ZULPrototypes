94ccce3cce325b10f274d80a51863234758af82c
shakti <svpsctg11@yahoo.com>
2018-08-10 18:07:40 +0530

    Refactor assesment action

--- src/components/report/AssesmentAction.js
+++ src/components/report/AssesmentAction.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View,StyleSheet } from 'react-native';
-import {Button} from 'native-base';
+import { Text, View, StyleSheet } from 'react-native';
+import { Button } from 'native-base';
 
 export default class AssesmentAction extends Component {
 
-    goToDashboard=()=>{
-      this.props.goToDashboard();
-  }
-  render() {
-    return (
-      <View style={styles.container}>    
-      <Button full  style={styles.mt15} onPress={this.goToDashboard}>
-            <Text style={{color:'white'}} >Go To Dashboard</Text>
-          </Button>
-      </View>
-    )
-  }
+    goToDashboard = () => {
+        this.props.goToDashboard();
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button full style={styles.mt15} onPress={this.goToDashboard}>
+                    <Text style={styles.textStyle} >Go To Dashboard</Text>
+                </Button>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-    mt15:{
-        marginBottom:20,
-        backgroundColor:'#5067ffd1'
+    mt15: {
+        marginBottom: 20,
+        backgroundColor: '#5067ffd1'
     },
     container: {
-        flex:1,
-        flexDirection:'column',
-        paddingTop:10
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: 10
     },
-    textStyle:{
-        color:'#222222',
+    textStyle: {
+        color: 'white',
 
     }
 });
