46d9d6075b1ad2799943265ae91fedf4a747092f
shakti <svpsctg11@yahoo.com>
2018-08-23 11:18:00 +0530

    Header changes in expert connect

--- src/components/expertconnect/ExpertConnect.js
+++ src/components/expertconnect/ExpertConnect.js
@@ -1,47 +1,110 @@
 import React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
-import { Container, Header, Item, Input, Icon, Button, Text, Content, Badge } from 'native-base';
+import { Container, Header, Item, Input, Icon, Button, Text, Content, Badge, Left, Body, Title, Right } from 'native-base';
+import IconBadge from 'react-native-icon-badge';
 import ExpertList from './ExpertList';
 
 
 const ExportConnect = (props) => {
+    let data = [
+        {
+            value: 'Parents Coach'
+        },
+        {
+            value: 'Marriage Counsellors'
+        },
+        ,
+        {
+            value: 'Search More'
+        }
+    ]
     return (
         <Container>
-            <Header searchBar rounded>
-                <Item>
-                    <Icon name="ios-search" />
-                    <Input placeholder="Search Experts" />
-                    <Icon name="ios-people" />
-                </Item>
-                <Button transparent>
-                    <Text>Search</Text>
-                </Button>
-            </Header>
-            <Content>
-                <ScrollView horizontal={true} style={{ padding: 5, flexDirection: 'row' }}>
-                    <Badge style={{ marginHorizontal: 5 }} primary>
-                        <Text>Available</Text>
+            {/* Expert Header*/}
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='arrow-left' type="FontAwesome" style={{ fontSize: 15 }} />
+                    </Button>
+                </Left>
+                <Body style={{ flexDirection: 'row' }}>
+                    <Text style={styles.header}>Parent's Coach <Icon name='chevron-down' type="FontAwesome" style={{ color: "white", fontSize: 15 }} /></Text>
+
+                </Body>
+                <Right style={{ flexDirection: 'row' }}>
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text style={styles.header}>Location</Text>
+                        <Text style={styles.header}>Online <Icon name='chevron-down' type="FontAwesome" style={{ color: "white", fontSize: 15 }} /></Text>
+                    </View>
+                </Right>
+
+
+            </Header >
+            {/* Filters Tags*/}
+            <View style={{ padding: 5, flexDirection: 'row' }} >
+                <ScrollView horizontal={true} style={{ padding: 5, flexDirection: 'row', flex: 1 }}>
+                    <Badge style={{ marginHorizontal: 5, backgroundColor: '#007bff' }}>
+                        <Text>Online</Text>
                     </Badge>
-                    <Badge style={{ marginHorizontal: 5 }} primary>
-                        <Text>Bengaluru</Text>
+                    <Badge style={{ marginHorizontal: 5, backgroundColor: '#007bff' }} >
+                        <Text>Family Specialist</Text>
                     </Badge>
-                    <Badge style={{ marginHorizontal: 5 }} primary>
-                        <Text>Online booking</Text>
-                    </Badge>
-                    <Badge style={{ marginHorizontal: 3, flexDirection: 'row', borderRadius: 5 }} primary>
-                        <Icon name="filter" type="FontAwesome" style={{ color: "#FFFFFF", fontSize: 15, marginTop: 4, marginLeft: 2 }} />
-                        <Text>ALL FILTERS</Text>
+                    <Badge style={{ marginHorizontal: 5, backgroundColor: '#6c757d' }}  >
+                        <Text>BTM Second Stage</Text>
                     </Badge>
                 </ScrollView>
+                <IconBadge
+                    MainElement={
+                    <View style={{width:40}}>    
+                        <Icon name="filter" type="FontAwesome" style={{ fontSize: 30, marginTop: 4, marginLeft: 2 }} />
+                    </View>
+                    
+                    }
+                    BadgeElement={
+                        <Text style={{ color: '#FFFFFF' }}>{3}</Text>
+                    }
+                    IconBadgeStyle={
+                        {
+                            width: 20,
+                            height: 20,
+                            backgroundColor: '#007bff'
+                        }
+                    }
+                    
+                />
+                
+            </View>
+            <Content >
                 <View style={{ backgroundColor: '#ddd' }}>
                     <ExpertList />
                 </View>
             </Content>
-        </Container>
+        </Container >
     )
 }
 const styles = StyleSheet.create({
     container: {
+        flexDirection: 'row',
+        flex: 1
+    },
+    header: {
+        // fontWeight: 'bold',
+        color: '#ffffff',
+        fontSize: 17
+    },
+    headerLeft: {
+        flex: 2,
+        justifyContent: 'flex-start'
+    },
+    headerRight: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    iconStyle: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+        marginTop: 3
     }
 })
 export default ExportConnect;
