76804612b25f1cfdab43a452c03a4ad4d2814e21
shakti <svpsctg11@yahoo.com>
2018-08-23 20:27:05 +0530

    stuff

--- src/components/expertconnect/SpecialityList.js
+++ src/components/expertconnect/SpecialityList.js
@@ -39,6 +39,11 @@ const ExpertList = (props) => {
                 </View>
             </View>
         </View>
+        {/*Find and connect*/}
+            <View style={{flexDirection:'row',padding:5,backgroundColor:'#fff',marginTop:5}}>
+                <Text style={{flex:1}}>Find & Connect</Text>
+                
+            </View>
             <FlatList
                 data={[{ key: 'a' }, { key: 'b' }, { key: 'a' }, { key: 'b' }, { key: 'a' }, { key: 'b' }]}
                 renderItem={({ item }) => <SpecialityComponent item={item} />}
@@ -57,66 +62,28 @@ const SpecialityComponent = () => {
         <View style={{ flexDirection: 'column', padding: 5, marginVertical: 3, backgroundColor: '#ffffff' }}>
             <View style={{ flex: 1, flexDirection: 'row' }}>
                 <View style={{ alignItems: 'center', padding: 5 }}>
-                    <Thumbnail large square source={require('../../assests/images/profilepic/stylish-girl.jpg')} />
-                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
-                        <Text style={{ color: '#00b386' }}>4.2</Text>
-                        <StarRating
-                            disabled={false}
-                            maxStars={1}
-                            rating={1}
-                            style={{ width: 50 }}
-                            fullStarColor={'#00b386'}
-                            starSize={18}
-                        />
-                    </View>
-                    <Text style={{ fontSize: 12, color: '#00b386', textDecorationLine: 'underline' }}>350 Reviews</Text>
-                    {/* <Text style={{ fontSize: 12, color: '#00b386', marginVertical: 5 }}>Available Today</Text> */}
+                    <Thumbnail medium square source={require('../../assests/images/profilepic/yogi.jpg')} />
                 </View>
                 <View style={{ flex: 1, paddingHorizontal: 10, paddingRight: 2 }}>
                     <View style={{ flexDirection: 'row', padding: 0, margin: 0 }}>
-                        <Text style={{ flex: 1 }}>Samira Reddy</Text>
-                        <Icon name='heart' type="FontAwesome" style={{ fontSize: 18, color: 'red', marginRight: 15, }} />
-                        <TouchableOpacity>
+                        <Text style={{ flex: 1 }}>Meditation Experts</Text>
+                        <Icon name='tripadvisor' type="FontAwesome" style={{ fontSize: 18, color: 'red',  }} />
+                        {/* <TouchableOpacity>
                             <Icon name='ellipsis-v' type="FontAwesome" style={{ fontSize: 18, }} />
-                        </TouchableOpacity>
+                        </TouchableOpacity> */}
                     </View>
-                    <Text style={{ fontSize: 13, color: '#505050', margin: 0 }}>Dermatologiest, MBBS</Text>
-                    <Text style={{ fontSize: 13, color: '#505050' }}>Child Specialists</Text>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 12 }}>5 Yrs Exp</Text>
-                    </View>
-                    <Text style={{ fontSize: 14, marginTop: 5 }}>Services available:</Text>
-                    <ScrollView horizontal={true} style={{ flexDirection: 'row' }}>
-                        <Badge style={{ backgroundColor: '#e8e8e8', borderRadius: 5, padding: 5, marginRight: 5 }}>
-                            <Text style={{ color: '#353535', fontSize: 14 }}>Skin therapy</Text>
+                    <Text style={{ fontSize: 14, marginTop: 2 }}>Services available:</Text>
+                    <ScrollView horizontal={true} style={{ flexDirection: 'row'}}>
+                        <Badge style={{ backgroundColor: '#ffffff', borderRadius: 5, padding: 5, marginRight: 5 ,borderWidth:1,borderColor:'#ddd'}}>
+                            <Text style={{ color: '#007bff', fontSize: 14 }}>Meditation Session</Text>
                         </Badge>
-                        <Badge style={{ backgroundColor: '#e8e8e8', borderRadius: 5, padding: 5, marginRight: 5 }}>
-                            <Text style={{ color: '#353535', fontSize: 14 }}>Laser treatment</Text>
+                        <Badge style={{ backgroundColor: '#ffffff', borderRadius: 5, padding: 5, marginRight: 5,borderWidth:1,borderColor:'#ddd' }}>
+                            <Text style={{ color: '#007bff', fontSize: 14 }}>Peaceful living</Text>
                         </Badge>
-                        <Badge style={{ backgroundColor: '#e8e8e8', borderRadius: 5, padding: 5, marginRight: 5 }}>
-                            <Text style={{ color: '#353535', fontSize: 14 }}>Hair Transplantation</Text>
-                        </Badge>
-                        <Badge style={{ backgroundColor: '#e8e8e8', borderRadius: 5, padding: 5, marginRight: 5 }}>
-                            <Text style={{ color: '#353535', fontSize: 14 }}>Fungal therapy</Text>
+                        <Badge style={{ backgroundColor: '#ffffff', borderRadius: 5, padding: 5, marginRight: 5 ,borderWidth:1,borderColor:'#ddd'}}>
+                            <Text style={{ color: '#007bff', fontSize: 14 }}>Life Advice</Text>
                         </Badge>
                     </ScrollView>
-                </View>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ flex: 1 }}>
-                    <TouchableOpacity style={{ paddingVertical: 4, backgroundColor: '#ffffff', borderWidth: 1, margin: 10, borderColor: '#ddd' }}>
-                        <Text style={{ textAlign: 'center' }}>Free <Icon name='wechat' type="FontAwesome" style={{ fontSize: 18, marginLeft: 5 }} /></Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 1 }}>
-                    <TouchableOpacity style={{ paddingVertical: 5, backgroundColor: '#ffffff', borderWidth: 1, margin: 10, borderColor: '#ddd' }}>
-                        <Text style={{ textAlign: 'center' }}>₹100 <Icon name='play-circle' type="FontAwesome" style={{ fontSize: 18, marginLeft: 5 }} /></Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flex: 1 }}>
-                    <TouchableOpacity style={{ paddingVertical: 5, backgroundColor: '#ffffff', borderWidth: 1, margin: 10, borderColor: '#ddd' }}>
-                        <Text style={{ textAlign: 'center' }}>₹300 <Icon name='calendar-plus-o' type="FontAwesome" style={{ fontSize: 18, marginLeft: 5 }} /></Text>
-                    </TouchableOpacity>
                 </View>
             </View>
         </View>
