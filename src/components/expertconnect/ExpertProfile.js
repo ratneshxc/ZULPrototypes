598a38b6d4d1b5f5841da44ab78b4656159de4ca
Shakti Chouhan <shakti.chouhan@pratian.com>
2018-08-28 12:26:34 +0530

    changes

--- src/components/expertconnect/ExpertProfile.js
+++ src/components/expertconnect/ExpertProfile.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import {  View } from 'react-native';
+import { Container, Header, Item, Input, Icon, Button, Text, Content, Badge, Left, Body, Title, Right,Toast } from 'native-base';
 
 export default class ExpertProfile extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Expert Profile </Text>
-      </View>
-    )
-  }
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='menu' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Expert Profile</Title>
+                    </Body>
+                    <Right>
+                    </Right>
+                </Header>
+                <Content padder style={{ backgroundColor: "white" }}>
+                </Content>
+            </Container>
+        )
+    }
 }
