9fe6e8f46ffa1e8acaa424a2d089c9357e789d11
ratneshxc <ratneshxc@gmail.com>
2018-09-05 21:30:33 +0530

    video changes

--- src/components/goals/GoalVideo.js
+++ src/components/goals/GoalVideo.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import { Platform, View, StyleSheet,Image, Text, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
+import { Platform, View, StyleSheet, Image, Text, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
 import { Footer, FooterTab, Card, Container, Header, Body, Left, Button, Icon, Right, Title, Content, Accordion, Badge, Segment, Item, Input } from 'native-base';
 import Video from 'react-native-video';
 
 const screenwidth = Dimensions.get('window').width - 20;
 
 class GoalVideo extends Component {
-   
+
     render() {
         return (
-            <View style={{backgroundColor:'#f0f0f0',}}>
-                <Text style={{ alignSelf:'center', color:'black' }}>Watch this video to complete your activity</Text>
-                <Image source={require("../../assests/icons/imagesVideo.jpeg") } style={{ alignSelf:'center'}} />
-                {/* <Video source={require("../../assests/videos/DSC_0082.mp4") }   // Can be a URL or a local file.
+            <View style={{ backgroundColor: '#f0f0f0', }}>
+                <Card style={{ padding: 5, backgroundColor: '#fff', marginVertical: 2 }}>
+                    <View style={{ flexDirection: 'row', marginBottom: 5 }}>
+                        <Text style={{ fontSize: 16, color: '#3a3a3a' }}>{'Daily 10 min meditation'}</Text>
+                    </View>
+                    <View>
+                        <View style={{ flexDirection: 'column', justifyContent: 'space-between' }}>
+                            <Text style={{ alignSelf: 'center', color: 'black' }}>Watch this video to complete your activity</Text>
+                            <Image source={require("../../assests/icons/imagesVideo.jpeg")} style={{ alignSelf: 'center' }} />
+                            {/* <Video source={require("../../assests/videos/DSC_0082.mp4") }   // Can be a URL or a local file.
                     ref={(ref) => {
                         this.player = ref
                     }}
@@ -23,15 +29,20 @@ class GoalVideo extends Component {
                     onEnd={this.onEnd}                      // Callback when playback finishes
                     onError={this.videoError}               // Callback when video cannot be loaded
                     style={styles.backgroundVideo} /> */}
+                        </View>
+
+                    </View>
+                </Card>
+
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
     backgroundVideo: {
-        height:220,
-        width:screenwidth,
-        alignSelf:'center'
-      },
+        height: 220,
+        width: screenwidth,
+        alignSelf: 'center'
+    },
 });
 export default GoalVideo;
