015791c3a246fd123fb12253ab4d21ca5e5b1bbe
Shakti Chouhan <shakti.chouhan@pratian.com>
2018-09-07 14:32:14 +0530

    remove commented code

--- src/components/expertconnect/ExpertList.js
+++ src/components/expertconnect/ExpertList.js
@@ -103,11 +103,9 @@ const ExpertComponent = (props) => {
             <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#ffffff',paddingBottom:2 }}>
                 <View style={{ alignItems: 'center', padding: 5 }}>
                     <Thumbnail medium round source={props.item.img} />
-                   
-                    {/* <Text style={{ fontSize: 12, color: '#00b386', marginVertical: 5 }}>Available Today</Text> */}
-                </View>
+                            </View>
                 <TouchableOpacity  style={{ flex: 2}} >
-                {/* onPress={()=>props.viewExperts()} */}
+           
                 <View style={{ flex: 2, paddingHorizontal: 10,paddingRight:2 }}>
                     <View style={{ flexDirection:'row',padding:0 ,margin:0 }}>
                         <Text style={{ flex: 1 ,marginLeft:10}}>{props.item.name}</Text>
@@ -127,37 +125,14 @@ const ExpertComponent = (props) => {
                         <View style={{flex:1}}>
                         </View>
                         <Button rounded primary style={{height:25,marginVertical:1}}  onPress={()=>props.bookExperts()} >
-                                {/* onPress={()=>props.goToBooking()} */}
+                      
                                     <Text>Connect</Text>
                                 </Button>
                     </View>
                     
-                    {/* <Text style={{ fontSize: 13, color: '#505050', margin:0}}>{props.item.degree}</Text> */}
-                   
-                    {/* <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 12 }}>{props.item.experience} Yrs Exp</Text>
-                    </View> */}
                 </View>
                 </TouchableOpacity>
-                {/* <View style={{ flex: 1, paddingHorizontal: 10,paddingRight:2 }}>
-                <View style={{ flexDirection: 'row', marginTop: 0 ,marginLeft:10}}>
-                    <Text style={{color:'#294787'}}>{props.item.rating}</Text>
-                        <StarRating
-                            disabled={false}
-                            maxStars={1}
-                            rating={1}
-                            style={{ width: 50 }}
-                            fullStarColor={'#294787'}
-                            starSize={18}
-                        />
-                    </View>
-                     <Text style={{ fontSize: 12, color: '#294787', marginLeft:10 }}>{props.item.reviews} Reviews</Text> 
-                    <View >
-                                <TouchableOpacity  style={{ paddingVertical: 4, backgroundColor:'white' , borderWidth: 1, margin: 0, marginLeft: 0, borderColor: '#294787', borderRadius: 50 }} onPress={()=>props.bookExperts()}>
-                                    <Text style={{ textAlign: 'center', color: '#294787' }}>Connect </Text>
-                                </TouchableOpacity>
-                            </View>
-                </View> */}
+
             </View>
 
         </View>
